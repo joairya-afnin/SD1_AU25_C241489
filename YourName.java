254a95d43dc9b53db404e3cf8d35f723a394f374
Joairya Afnin Johana <joairyaafnin194@gmail.com>
2025-09-20 04:23:05 +0600

    Add files via upload

---
+++ YourName.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author LENOVO
+ */
+import java.util.Scanner;
+
+public class YourName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        
+        System.out.println("Hello, " + name + "!");
+    }
+}
